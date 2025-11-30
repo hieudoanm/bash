8dbb6abfb781c8682c5e4e7bb6fece574f8a6806
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-30 23:00:10 +0700

    add function

--- packages/cli/src/macos.bash
+++ packages/cli/src/macos.bash
@@ -14,3 +14,13 @@ function print-env() {
 alias delete-ds-store="find . -name ".DS_Store" -delete"
 alias kill-port='sudo lsof -i tcp:'
 alias hex='openssl rand -hex 32'
+
+list_packages() {
+  for d in */; do
+    if [[ -d "$d/packages" ]]; then
+      echo "[$d]"
+      find "$d/packages" -mindepth 1 -maxdepth 1 -type d | sort
+      echo
+    fi
+  done
+}
